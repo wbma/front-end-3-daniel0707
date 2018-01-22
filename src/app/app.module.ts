4982c3acf1acb77776d38afb42eba94e1ad11548
Daniel Vahla <vahla.daniel@gmail.com>
2018-01-22 15:34:29 +0200

    part 1 + extra

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,20 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-
+import { ListMediaComponent } from './list-media/list-media.component';
+import { MediaService } from './services/media.service';
+import {HttpClientModule} from '@angular/common/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListMediaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, HttpClientModule, NgbModule.forRoot()
   ],
-  providers: [],
+  providers: [MediaService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
